227d24cd284ebcc6b660f069b3644d1cf186d0f2
vikas pal <110290006+vikas83pal@users.noreply.github.com>
2023-09-25 21:02:51 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class DataTypes {
+     public static void main(String[] args){
+          Scanner scn=new Scanner(System.in);
+          System.out.println("Enter The a Value:");
+          int a=scn.nextInt();
+          System.out.println(a);
+     }
+}
