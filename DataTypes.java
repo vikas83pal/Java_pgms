687cadba5664648f1d2fa4570744a1f36e392f61
vikas pal <110290006+vikas83pal@users.noreply.github.com>
2023-09-26 21:00:50 +0530

    Update DataTypes.java

--- DataTypes.java
+++ DataTypes.java
@@ -3,11 +3,24 @@ import java.util.Scanner;
 public class DataTypes {
      public static void main(String[] args){
           Scanner scn=new Scanner(System.in);
-          System.out.println("Enter The a Value:");
+          
+          System.out.println("Enter The integer Value:");
           int a=scn.nextInt();
-          System.out.println(a);
-           System.out.println("Enter The b Value:");
-          int a=scn.nextInt();
-          System.out.println(b);
+          
+          System.out.println("Enter The float Value:");
+          float f=scn.nextFloat();
+          
+          System.out.println("Enter The Double Value:");
+          double d=scn.nextDouble();
+          
+          System.out.println("Enter The string Value:");
+          String str=scn.next();
+          
+          System.out.println("The Integer Value is :"+a);
+          System.out.println("The Float Value is :"+f);
+          System.out.println("The Double Value is :"+d);
+          System.out.println("The String Value is :"+str);
+
+          
      }
 }
