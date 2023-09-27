d0492516429052e87789737e013cde66eccd93fe
vikas pal <110290006+vikas83pal@users.noreply.github.com>
2023-09-27 20:01:07 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,10 @@
+public class Factorial {
+     public static void main(String[] args){
+          int fact=1;
+          int num=5;
+          for(int i=1;i<=num;i++){
+               fact*=i;
+          }
+          System.out.println("Factorial Of 5 Is : "+fact);
+     }
+}
