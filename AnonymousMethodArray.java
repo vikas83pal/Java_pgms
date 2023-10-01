bc2bfad7df015ad3ae46e5a3f8ea71021aa0b891
vikas pal <110290006+vikas83pal@users.noreply.github.com>
2023-10-01 21:13:31 +0530

    Update AnonymousMethodArray.java

--- AnonymousMethodArray.java
+++ AnonymousMethodArray.java
@@ -1,13 +1,30 @@
+// class Demo{
+//      public static void printArray(int[] arr){
+//           for(int num:arr){
+//                System.out.print(num+" ");
+//           }
+//      }
+// }
+// public class AnonymousMethodArray {
+//      public static void main(String[] args){
+//           int[] arr={1,2,3,4,5};
+//           Demo.printArray(arr);
+//      }
+// }
+
+
+//Reruning The Array Method
 class Demo{
-     public static void printArray(int[] arr){
-          for(int num:arr){
-               System.out.print(num+" ");
-          }
+     public static int[] printArray(int[] arr){
+          return arr;
      }
 }
 public class AnonymousMethodArray {
      public static void main(String[] args){
           int[] arr={1,2,3,4,5};
           Demo.printArray(arr);
+          for(int num:arr){
+               System.out.print(num+" ");
+          }
      }
 }
